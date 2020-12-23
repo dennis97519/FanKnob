d572f91c30530ff61bbc716527970a4789d4e491
dennis97519 <4066022+dennis97519@users.noreply.github.com>
2020-12-22 22:42:15 -0600

    work around glitch

--- potToPWM/potToPWM.ino
+++ potToPWM/potToPWM.ino
@@ -4,6 +4,22 @@ void tachISR(){
   tachCount+=1;
 }
 
+
+// disable timer output on OC1B
+void oc1bDis(){
+  TCCR1A &= ~(_BV(COM1B1)|_BV(COM1B0));
+}
+
+// enable output on OC1B
+void oc1bEn(){
+  oc1bDis();
+  //Compare output mode: non-inverting output on OC1B: COM1B=0b10
+  TCCR1A |= _BV(COM1B1);
+}
+
+bool isOc1bEn(){
+  return (TCCR1A & _BV(COM1B1)) || (TCCR1A & _BV(COM1B0));
+}
 unsigned long lastTachTime;
 void setup() {
   // put your setup code here, to run once:
@@ -32,8 +48,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  OCR1B=map(analogRead(A0),0,1024,0,639);
+  int potval=analogRead(A0);
+  OCR1B=map(potval<50?0:(potval>1014?1023:potval),0,1023,0,639);
+  if(OCR1B==0 && isOc1bEn()){
+    // timer glitches high for a moment when OCR1B is 0
+    digitalWrite(10,LOW);// OC1B is pin 10
+    // digitalWrite automatically disables fast PWM (clears COM1B1)
+    // https://garretlab.web.fc2.com/en/arduino/inside/hardware/arduino/avr/cores/arduino/wiring_digital.c/digitalWrite.html
+    // https://garretlab.web.fc2.com/en/arduino/inside/hardware/arduino/avr/cores/arduino/wiring_digital.c/turnOffPWM.html
+  }
+  else if(OCR1B!=0 && !isOc1bEn()){
+    oc1bEn();
+  }
   if(millis()-lastTachTime>=1000){
+    // for tachometer, 1k pull up and 100nf capacitor 
+    // seems to clean up the signal enough for this to work
     noInterrupts();
     unsigned long timePassed=millis()-lastTachTime;
     unsigned long rpm = 60000*tachCount/timePassed/2;
